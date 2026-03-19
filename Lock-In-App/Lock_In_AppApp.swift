10dfd7815f6b336efcb0745b5c671721b580866d
Dominik Pathuis <dominikpathuis@gmail.com>
2026-03-19 14:34:46 -0400

    Messed up with the last repo. Made a new empty project to push and I will copy in the code into this new repo

---
+++ Lock-In-App/Lock_In_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lock_In_AppApp.swift
+//  Lock-In-App
+//
+//  Created by Dominik Pathuis on 3/19/26.
+//
+
+import SwiftUI
+
+@main
+struct Lock_In_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
